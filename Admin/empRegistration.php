9cfb95d9decc941457f2a1b205debb59fcf99655
2014bcs010 <openjamosesopm@gmail.com>
2018-06-04 20:39:08 +0300

    Updates

--- Admin/empRegistration.php
+++ Admin/empRegistration.php
@@ -1,18 +1,18 @@
 
 <?php
-   include('../config.php');
 
-$sql="INSERT INTO employee (Employee_Name, Username, Password,Picture) 
-VALUES ('$_POST[fullname]', '$_POST[username]', '$_POST[password]', '$_POST[picture]')";
+include('../config.php');
+$role = "B-Owner";
+$sql = "INSERT INTO employee (Employee_Name, Username, Password,role,mtn,airtel,Picture) 
+VALUES ('$_POST[fullname]', '$_POST[username]', '$_POST[password]','$role','$_POST[mtn]','$_POST[airtel]', '$_POST[picture]')";
 
-if (!mysqli_query($mysqli,$sql))
-  {
-  die('Error: ' . mysqli_error($mysqli));
-  }
-   header("location: Employee.php");
-  echo "1 record added";
+if (!mysqli_query($mysqli, $sql)) {
+    die('Error: ' . mysqli_error($mysqli));
+}
+header("location: Employee.php");
+echo "1 record added";
 
- mysqli_close($mysqli);
+mysqli_close($mysqli);
 ?> 
 
 
