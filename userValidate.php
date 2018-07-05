58c7a39a97c4b600f08a2d957713c3bd9bc8b9b1
2014bcs010 <openjamosesopm@gmail.com>
2018-07-05 03:23:54 +0300

    Updates

--- userValidate.php
+++ userValidate.php
@@ -13,9 +13,38 @@ session_start(); {
     $magaca = mysqli_real_escape_string($mysqli, $magaca);
     $furaha = mysqli_real_escape_string($mysqli, $furaha);
 
-    $fetch = mysqli_query($mysqli, "select Cust_Id from customer where Email='$magaca' and Password= '$furaha'");
-    $count = mysqli_num_rows($fetch);
+    //$fetch = mysqli_query($mysqli, "select Cust_Id from customer where Email='$magaca' and Password= '$furaha'");
+    //$count = mysqli_num_rows($fetch);
     
+    $results = mysqli_query($mysqli, "select * from customer where (Email or UserName='$magaca') and Password = '$furaha' order by Cust_Id desc limit 1 ");
+    //$count = mysqli_num_rows($fetch);
+
+    //$results = $mysqli->query("SELECT * FROM payment ORDER BY order_ID DESC LIMIT 1");
+    if ($results) {
+        //fetch results set as object and output HTML
+        if ($obj = $results->fetch_object()) {
+            //$last_id = $obj->order_ID;
+            $_SESSION['login_username'] = $magaca;
+            $check=$_SESSION['login_username'];
+            $_SESSION['customer_id'] = $obj->Cust_Id;
+            $_SESSION['FullName'] = $obj->FullName;
+            $_SESSION['Phone'] = $obj->Phone;
+            $_SESSION['Email'] = $obj->Email;
+            $_SESSION['Password'] = $obj->Password;
+            $_SESSION['Country'] = $obj->Country;
+            $_SESSION['City'] = $obj->City;
+            $_SESSION['Adress'] = $obj->Adress;
+            $_SESSION['PostalCode'] = $obj->PostalCode;
+           
+            
+            header("location: Admin/index.php");
+        } else {
+            header('Location: Sign In.php');
+        }
+    } else {
+        header('Location: Sign In.php');
+    }
+    /**
     if ($count != "") {
         $_SESSION['login_username'] = $magaca;
         $check=$_SESSION['login_username'];
@@ -24,5 +53,7 @@ session_start(); {
     } else {
         header('Location: Sign In.php');
     }
+     * 
+     */
 }
 ?>
